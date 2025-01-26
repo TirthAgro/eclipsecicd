9c34e2814560ffef4eb6bb3b716abca0b6a4d792
tirth <sam@shaktimanagro.com>
2025-01-26 14:23:01 +0530

    Third update

--- src/main/java/firstapp/first/Welcome.java
+++ src/main/java/firstapp/first/Welcome.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Welcome {
     @RequestMapping("/welcome")
     public String Welcome(){
-        return "Hi , this is my first app";
+        return "HHwllo Sonu , successed ";
 
 
     }
