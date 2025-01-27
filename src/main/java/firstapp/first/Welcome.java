8ef6f4135488f7cdc24643d4ec687ee7ebbe12e1
tirth <sam@shaktimanagro.com>
2025-01-27 12:05:00 +0530

    Nishant update

--- src/main/java/firstapp/first/Welcome.java
+++ src/main/java/firstapp/first/Welcome.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Welcome {
     @RequestMapping("/welcome")
     public String Welcome(){
-        return "Hello Nishant , This is your fifth app1";
+        return "Hello Nishant , This is your fifth app2";
 
 
     }
