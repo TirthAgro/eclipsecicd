9b67215274540bb7fd69898a72cf9a438409f29a
TirthAgro <sam@shaktimanagro.com>
2025-02-15 10:58:07 +0530

    Update Welcome.java
    Update on 15 feb

--- src/main/java/firstapp/first/Welcome.java
+++ src/main/java/firstapp/first/Welcome.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Welcome {
     @RequestMapping("/welcome")
     public String Welcome(){
-        return "Hello TATPL1";
+        return "Hello TATPL2";
 
 
     }
